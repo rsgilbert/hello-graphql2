9ba3c6b715d0961f9c42778a69ffb5cdc798b8a3
Gilbert <gssenyonjo@gmail.com>
2019-12-26 18:44:12 +0300

    implemented graphql links, the schema and the resolver. Working well. Understanding bit by bit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga')
 
+// schema
 const typeDefs = `
     type Query {
         info: String!
+        feed: [Link]!
+    }
+   
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }
 `;
 
+let links = [
+    {
+        id: 'link-0',
+        url: 'www.howtographql.com',
+        description: 'Fullstack tutorial for GraphQL'
+    }, {   
+        id:  7,
+        url: 'www.campusgram.me',
+        description: 'Being Polite'
+    }
+]
+
+// decides where the data will be got from
 const resolvers = {
     Query: {
-        info: () => `Simple graphql API`
+        info: () => `Simple graphql API`,
+        feed: () => links
+    
     }
 }
 
